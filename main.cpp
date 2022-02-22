a173c3cb845c4e76dd2949ea218886c4719fe33a
BabiyDariya <90989214+BabiyDariya@users.noreply.github.com>
2022-02-22 18:46:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "functions.h"
 
-
-int main()
+float findMinValue(float n1, float n2, float n3);
 {
-	//TODO: if you want to test your functions, write your code here
-	
+float min = n1;
+  if (n2 <= min) min = n2;
+  if (n3 <= min) min = n3;
+
+  return min;
+}
 	
 	
 	return 0;
