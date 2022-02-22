69127f577a8ccb96792e62eb8663a9c2d9521d07
BabiyDariya <90989214+BabiyDariya@users.noreply.github.com>
2022-02-22 18:43:58 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "functions.h"
 
 
 float findMinValue(float n1, float n2, float n3);
@@ -7,7 +7,6 @@ float findMinValue(float n1, float n2, float n3);
 float a = n1;
   if (n2 <= a) a = n2;
   if (n3 <= a) a = n3;
-  
-  cout << "minimum: " <<a<<end1;
+
   return a;
 }
