0de26d680c8b0274b6a7880e75e578003f832696
BabiyDariya <90989214+BabiyDariya@users.noreply.github.com>
2022-02-22 18:45:16 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -4,9 +4,9 @@
 
 float findMinValue(float n1, float n2, float n3);
 {
-float a = n1;
-  if (n2 <= a) a = n2;
-  if (n3 <= a) a = n3;
+float min = n1;
+  if (n2 <= min) min = n2;
+  if (n3 <= min) min = n3;
 
-  return a;
+  return min;
 }
